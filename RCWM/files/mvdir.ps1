656faccae00348d173a2f1c7feae1072ecc3e8b2
GChuf <gasper.cefarin@gmail.com>
2020-10-24 22:07:26 +0200

    Add mvdir, improve rcopy

--- RCWM/files/mvdir.ps1
+++ RCWM/files/mvdir.ps1
@@ -1,3 +1,3 @@
 $n = -join ((0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f') | get-random -count 6)
 $p = (get-location).path
-$p | out-file C:\windows\system32\rcwm\rc\$n -encoding UTF8 -NoNewline
+$p | out-file C:\windows\system32\rcwm\mv\$n -encoding UTF8 -NoNewline
