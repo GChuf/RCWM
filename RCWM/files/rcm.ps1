f002208daabf7f13940df0418d6a75fe0c1c6986
GChuf <gasper.cefarin@gmail.com>
2020-10-24 22:07:26 +0200

    Add mvdir, improve rcopy
    Former-commit-id: 656faccae00348d173a2f1c7feae1072ecc3e8b2

---
+++ RCWM/files/rcm.ps1
@@ -0,0 +1,175 @@
+#flags used when robocopying (overwrites files):
+
+#/E :: copy subdirectories, including Empty ones
+#/NP :: No Progress - don't display percentage copied
+#/NJH :: No Job Header
+#/NJS :: No Job Summary
+#/NC :: No Class - don't log file classes
+#/NS :: No Size - don't log file sizes
+#/MT[:n] :: Do multi-threaded copies with n threads (default 8)
+#/MOVE :: MOVE files AND dirs (delete from source after copying).
+
+#when merging, these are added to not overwrite any files:
+
+#/XC :: eXclude Changed files.
+#/XN :: eXclude Newer files.
+#/XO :: eXclude Older files.
+ 
+
+
+#todo
+#check if file exists
+#check if source folder exists
+
+#check if list of folders to be copied exist
+
+If (  (Test-Path "C:\Windows\System32\RCWM\mv\*" -PathType Leaf) -eq $false ) {
+	echo "List of folders to be copied does not exist!"
+	Start-Sleep 1
+	echo "Create one by right-clicking on folders and selecting 'Move this Directory'."
+	Start-Sleep 3
+	#exit
+}
+
+
+#set high process priority
+$process = Get-Process -Id $pid
+$process.PriorityClass = 'High' 
+
+
+#get cd
+$BaseDir = (pwd).path
+$BaseDirDisp = '"'
+$BaseDirDisp += $BaseDir
+$BaseDirDisp += '"'
+
+#get array of contents of files inside C:\windows\system32\RCWM\mv (that would be the pathnames to move)
+$files = get-childitem "C:\windows\system32\RCWM\mv"
+[string[]]$array = Get-Content $files.fullName
+
+
+
+If ( $array.length -eq 1 ) {
+	Write-host "You're about to move the following folder into" $BaseDirDisp":"
+} else {
+	Write-host "You're about to move the following" $array.length "folders into" $BaseDirDisp":"
+}
+	$array
+
+#Prompt
+Do {
+	$Valid = $True
+	[string]$prompt = Read-Host -Prompt "Is this okay? (Y/N)"
+	Switch ($prompt) {
+
+		default {
+			Write-Host "Not a valid entry."
+			$Valid = $False
+		}	
+
+		{"y", "yes" -contains $_} {
+			$copy = $True
+		}
+		
+		{"n", "no" -contains $_} {
+	
+			Do {
+				[string]$prompt = Read-Host -Prompt "Delete list of folders? (Y/N)"
+				Switch ($prompt) {
+				
+					default {
+						Write-Host "Not a valid entry."
+						$Valid = $False
+					}	
+					
+					{"y", "yes" -contains $_} {
+						Remove-Item C:\Windows\System32\RCWM\mv\*
+						Write-Host "List deleted."
+						Start-Sleep 2
+						exit
+					}
+					
+					{"n", "no" -contains $_} {
+						Write-Host "Aborting."
+						Start-Sleep 3
+						exit
+					}
+	
+				}
+			} Until ($Valid)		
+			
+		}
+	}
+} Until ($Valid)
+
+
+
+
+If ( $copy -eq $True ) {
+
+	write-host "Begin moving ..."
+	write-host ""
+	
+
+	for ($i=0; $i -lt $array.length; $i++) {
+
+		#get folder path
+		$path = $array[$i]
+
+
+		#get folder name
+		$folder = $path.split("\")[-1]
+
+	#todo
+	#does source folder exist? do i need to check?
+
+
+
+		#if exist folder
+		
+		If (Test-Path $basedir\$folder) {
+			#store folders for merge prompt
+			#overwrite - or just copy
+			[string[]]$merge += $path
+		} else {
+			#todo -to so 3 linije zdej
+			#echo "Copying" $path "..."
+			
+			#make new directory with the same name as the folder being copied
+
+			New-Item -Path ".\$folder" -ItemType Directory | Out-Null
+
+			C:\Windows\System32\robocopy.exe "$path" "$folder" /MOVE /E /NP /NJH /NJS /NC /NS /MT:32
+			
+			#delete first line here?
+			#(Get-Content C:\Windows\System32\RCWM\mv.log | Select-Object -Skip 1) | Set-Content C:\Windows\System32\RCWM\mv.log		
+			#Start-Sleep 20		
+			
+			#todo
+			#should I output everything into one file so I can delete first lines????
+			#no ... user will be asked to merge anyway.
+			echo "Finished copying $folder"				
+		}
+
+	}	
+
+		#if merge array exists
+		if ($merge) {
+		
+			Write-host "Successfully moved" $($array.length - $merge.length) "out of" $array.length "folders."
+			#todo merge acts like before array
+			Write-host "The following" $merge.length "folders already exist inside" $BaseDirDisp":"
+			$merge
+			Write-host "Cannot move these files! Deleting the list of files ..."
+			Remove-Item C:\Windows\System32\RCWM\mv\*		
+			Start-Sleep 3		
+		}
+
+
+
+	echo ""
+	echo "Finished!"
+	Remove-Item C:\Windows\System32\RCWM\mv\*
+	Start-Sleep 2
+
+}
