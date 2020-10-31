2b6475136cf7472a260044dcecb1e956a4b181b0
GChuf <gasper.cefarin@gmail.com>
2020-10-31 18:01:27 +0100

    Optimize powershell files
    Former-commit-id: 18c494e89b09f52fc338c2d9ce03c5dd3829860a

--- RCWM/files/rcopy.ps1
+++ RCWM/files/rcopy.ps1
@@ -1,3 +1,2 @@
-$n = -join ((0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f') | get-random -count 6)
-$p = (get-location).path
-$p | out-file C:\windows\system32\rcwm\rc\$n -encoding UTF8 -NoNewline
+$n = -join ((0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f')|get-random -count 6)
+(get-location).path|out-file C:\windows\system32\rcwm\rc\$n -encoding UTF8 -NoNewline
