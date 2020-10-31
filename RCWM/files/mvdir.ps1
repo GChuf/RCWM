e3425a63e319a142ab9ee486a6c1f3f5b2e5c1e4
Chuf <42591821+GChuf@users.noreply.github.com>
2020-10-31 19:24:23 +0100

    Fix bug
    Former-commit-id: 579fbb083c6844e669462bd1b862a0f98e9b3aab

--- RCWM/files/mvdir.ps1
+++ RCWM/files/mvdir.ps1
@@ -1,2 +1,2 @@
 $n = -join ((0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f')|get-random -count 6)
-$p = (get-location).path|out-file C:\windows\system32\rcwm\mv\$n -encoding UTF8 -NoNewline
+(get-location).path|out-file C:\windows\system32\rcwm\mv\$n -encoding UTF8 -NoNewline
