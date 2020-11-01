a9763e1b2ae03068bcea8e22c79b279dcdc724f3
Chuf <42591821+GChuf@users.noreply.github.com>
2020-11-01 01:43:26 -0800

    Update random function in ps files
    Speeds up random function in powershell files, uses only numbers
    
    
    Former-commit-id: 75defa1a3381f801179883950c693e2b310bb354

--- RCWM/files/mvdir.ps1
+++ RCWM/files/mvdir.ps1
@@ -1,2 +1,1 @@
-$n = -join ((0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f')|get-random -count 6)
-(get-location).path|out-file C:\windows\system32\rcwm\mv\$n -encoding UTF8 -NoNewline
+(get-location).path|out-file C:\windows\system32\rcwm\mv\$(get-random -maximum 0x8FFFFF) -encoding UTF8 -NoNewline
