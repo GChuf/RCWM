52e3c1db8a6dda3f45cb0b2da6a41aab78e9e44c
GChuf <gasper.cefarin@gmail.com>
2021-01-03 17:22:06 +0100

    Fix links for files
    Former-commit-id: 314b4e89ac2dc0a597a79912f2b2e0ec6c97bb87

--- RCWM/files/SymbolicLinkFile.bat
+++ RCWM/files/SymbolicLinkFile.bat
@@ -10,46 +10,41 @@ exit
 )
 
 :start
+echo 1
 wmic process where name="cmd.exe" CALL setpriority 256 >nul
 wmic process where name="conhost.exe" CALL setpriority 256 >nul
 set curdir=%cd%
-set /P folder=<C:\Windows\System32\RCWM\fl.log
+set /P file=<C:\Windows\System32\RCWM\fl.log
+IF NOT EXIST "%file%" (echo Link Source does not exist! && timeout /t 1 >nul && echo Exiting . . . && timeout /t 1 > nul && exit)
 
-IF NOT EXIST "%folder%" (echo Link Source does not exist! && timeout /t 1 >nul && echo Exiting . . . && timeout /t 1 > nul && exit)
+for %%F in ("%file%") do set f=%%~nxF
 
-cd /d "%folder%"
-for %%I in (.) do set fname=%%~nxI
-cd /d "%curdir%"
-
-IF EXIST "%fname%" (
+IF EXIST "%f%" (
 goto :f1
 ) ELSE (
 goto :f2
 )
-
 :f1
-IF EXIST "%fname%\" (
+IF EXIST "%f%\" (
 echo Folder with the same name already exists!
 echo Cannot continue!
-timeout /t 4
+timeout /t 3
 exit
 ) ELSE (
 echo File with the same name already exists!
 echo Cannot continue!
-timeout /t 4
+timeout /t 3
 exit
-pause
-exit
+
 )
 
 :f2
 echo.
 echo Creating symbolic link . . .
 echo.
-
-mklink "%curdir%\%fname%" "%folder%"
-
+mklink "%curdir%\%f%" "%file%"
 del /f /q C:\Windows\System32\RCWM\fl.log
 echo Finished!
 timeout /t 1 1>NUL
+pause
 exit
