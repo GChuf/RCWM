69c8554a567ca70cf1f1117da386b24a49823a01
GChuf <gasper.cefarin@gmail.com>
2021-11-28 18:26:21 +0100

    change priority to high, disable outputting empty lines

--- RCWM/files/RCopy.bat
+++ RCWM/files/RCopy.bat
@@ -1,28 +1,29 @@
 @echo off
-FOR /F "tokens=*" %%g IN ('powershell "((Get-ItemProperty HKCU:\RCWM\rc | out-string -stream) | ? {$_.trim() -ne \"\" }).length"') do (SET E=%%g)
 
+rem 65000: UTF-7
+rem 65001: UTF-8 does not work on Win7
 chcp 65001 > nul
+
+FOR /F "tokens=*" %%g IN ('powershell "((Get-ItemProperty HKCU:\RCWM\rc | out-string -stream) | ? {$_.trim() -ne \"\" }).length"') do (SET E=%%g)
 
 IF %E% == 0 (
 echo Source folder not specified!
 echo Right-Click and 'RoboCopy' a folder.
 timeout /t 3 > nul
-
 exit
 ) ELSE (
 goto start )
 
 :start
 
-rem wmic process where name="cmd.exe" CALL setpriority 256 1>NUL
-rem wmic process where name="conhost.exe" CALL setpriority 256 1>NUL
+wmic process where name="cmd.exe" CALL setpriority 128 2>nul 1>nul
+wmic process where name="conhost.exe" CALL setpriority 128 2>nul 1>nul
 
 set curdir=%cd%
 
-
 FOR /F "tokens=*" %%g IN ('powershell "((Get-ItemProperty HKCU:\RCWM\rc | out-string -stream) | ? {$_.trim() -ne \"\" } | select -first 1) -replace \".{3}$\""') do (SET folder=%%g)
 
-IF NOT EXIST "%folder%" (echo Source folder does not exist: %folder% && timeout /t 1 >nul && echo Exiting . . . && timeout /t 2 > nul && exit)
+IF NOT EXIST "%folder%" (echo Source folder does not exist: %folder% && timeout /t 1 >nul && echo Exiting . . . && timeout /t 2 > nul && exit )
 cd /d %folder%
 for %%I in (.) do set fname=%%~nxI
 cd /d "%curdir%"
