8eb898b4b1930f04cc8af9d216d6a7c090230b22
Gašper Čefarin <gasper.cefarin@actual-it.si>
2022-02-15 15:45:26 +0100

    Improve saving paths process for multiple files
    How did I miss this anyway

--- RCWM/files/rcopy.ps1
+++ RCWM/files/rcopy.ps1
@@ -1,1 +1,1 @@
-(get-location).path|out-file C:\windows\system32\rcwm\rc\$(get-random -maximum 0x98967F) -encoding UTF8 -NoNewline
+$args[0]|out-file C:\windows\system32\rcwm\rc\$(get-random -maximum 0x98967F) -encoding UTF8 -NoNewline
