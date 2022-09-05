0e921497e3d2f8e36e9d808ecbc0961d560d1a07
GChuf <gasper.cefarin@gmail.com>
2022-09-05 17:08:03 +0100

    Powershell 2 unicode fix - WIP

--- RCWM/install.cmd
+++ RCWM/install.cmd
@@ -60,10 +60,11 @@ cd files
 IF EXIST "C:\Program Files\PowerShell\7" (
     FOR /F "tokens=* USEBACKQ" %%F IN (`pwsh -command $psversiontable.psversion.major`) DO ( SET pwsh7=%%F )
 	rem if using pwshv7, replace powershell -> pwsh + delete execution policy which doesnt work anymore
-	powershell -command "(Get-Content .\MvDirSingle.reg) -Replace 'powershell Set-ExecutionPolicy Bypass -Scope Process;', 'pwsh' | Set-Content .\MvDirSingle.reg"
-	powershell -command "(Get-Content .\MvDirMultiple.reg) -Replace 'powershell Set-ExecutionPolicy Bypass -Scope Process;', 'pwsh' | Set-Content .\MvDirMultiple.reg"
-	powershell -command "(Get-Content .\RCopySingle.reg) -Replace 'powershell Set-ExecutionPolicy Bypass -Scope Process;', 'pwsh' | Set-Content .\RCopySingle.reg"
-	powershell -command "(Get-Content .\RCopyMultiple.reg) -Replace 'powershell Set-ExecutionPolicy Bypass -Scope Process;', 'pwsh' | Set-Content .\RCopyMultiple.reg"
+	rem powershell -command "(Get-Content .\MvDirSingle.reg) -Replace 'powershell Set-ExecutionPolicy Bypass -Scope Process;', 'pwsh' | Set-Content .\MvDirSingle.reg"
+	xcopy .\pwsh7\MvDirSingle.reg . /Y > nul
+	xcopy .\pwsh7\MvDirMultiple.reg . /Y > nul
+	xcopy .\pwsh7\RCopySingle.reg . /Y > nul
+	xcopy .\pwsh7\RCopyMultiple.reg . /Y > nul
 	goto pwsh7
 ) ELSE (
 	goto pwsh
