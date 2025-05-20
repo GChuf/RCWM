c54a43c615b857d4b423387e3a5bf61bac0f42f4
GChuf <gasper.cefarin@gmail.com>
2025-05-20 16:31:57 +0200

    accept 2 arguments

--- RCWM/files/ExecutionFiles/RcwmExecutable.cpp
+++ RCWM/files/ExecutionFiles/RcwmExecutable.cpp
@@ -1,5 +1,6 @@
 #include <windows.h>
 #pragma comment(lib, "Advapi32.lib")
+#include <string>
 /*
 void LogToFile(const char* message) {
     const char* logPath = "C:\\Users\\root\\Desktop\\reg_log.txt";
@@ -13,12 +14,19 @@ void LogToFile(const char* message) {
     }
 }
 */
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR path, int) {
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR arg, int) {
     // Registry operations
     //HKEY hKey;
     HKEY hSubKey;
 
-    const char* keyPath = "Software\\RCWM\\dl";
+
+    std::string fullArg(arg);
+
+    std::string regKey = fullArg.substr(0, 2);
+    std::string directoryPath = fullArg.substr(3); // everything after first 2 chars
+
+    std::string regKeyFullPath = "Software\\RCWM\\" + regKey;
+    //const char* regKeyFullPath = "Software\\RCWM\\dl";
 
     /*
     LONG result = RegCreateKeyExA(HKEY_CURRENT_USER, keyPath, 0, NULL, 0, KEY_WRITE, NULL, &hKey, NULL);
@@ -35,8 +43,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR path, int) {
     }
     */
 
-    RegOpenKeyA(HKEY_CURRENT_USER, keyPath, &hSubKey);
-    RegSetValueExA(hSubKey, path, 0, REG_NONE, NULL, 0);
+    RegOpenKeyA(HKEY_CURRENT_USER, regKeyFullPath.c_str(), &hSubKey);
+    RegSetValueExA(hSubKey, directoryPath.c_str(), 0, REG_NONE, NULL, 0);
     //LogToFile(result == ERROR_SUCCESS ? "Registry write successful" : "Registry write failed");
     RegCloseKey(hSubKey);
 
