b2fc534049c04d9a943aa223d0f8ee75d59f0051
GChuf <gasper.cefarin@gmail.com>
2025-07-10 23:00:58 +0200

    wip

--- RCWM/files/ExecutionFiles/RcwmExecPowershell.cpp
+++ RCWM/files/ExecutionFiles/RcwmExecPowershell.cpp
@@ -1,18 +1,34 @@
 #include <windows.h>
+#include <string>
 
-int main() {
+// cl /O2 /Os /nologo /Fe:rcwm-ps1.1.exe RcwmExecPowershell.cpp /link  /OPT:REF /OPT:ICF Advapi32.lib  /SUBSYSTEM:CONSOLE
+
+int main(int argc, char* argv[]) {
+
+    //argc always 3
+    //third argument (path) needs to be inside double quotes, because arguments are delimited by spaces
+    // 
+    // 
+    // 
+    std::string method = argv[1];
+    std::string mode = argv[2];
+    std::string path = argv[3];
+
+
     // Your command (e.g., write to file)
-    const char* cmd = "cmd.exe /c powershell -NoLogo -NoProfile -ExecutionPolicy Bypass -File C:\\git\\rcwm\\RCWM\\files\\ExecutionFiles\\rcp.ps1\"";
+    //const char* cmd = "cmd.exe /c powershell -NoLogo -NoProfile -ExecutionPolicy Bypass -File C:\\git\\rcwm\\RCWM\\files\\ExecutionFiles\\rcp.ps1" + method + " " + mode + " " + path;
+    std::string cmd = "cmd.exe /c powershell -NoLogo -NoProfile -ExecutionPolicy Bypass -File C:\\git\\rcwm\\RCWM\\files\\ExecutionFiles\\rcp.ps1 " + method + " " + mode + " " + path;
+    const char* command = cmd.c_str();
 
-    // Start hidden process
+    // Start process
     STARTUPINFOA si = { sizeof(si) };
     PROCESS_INFORMATION pi;
     si.dwFlags = STARTF_USESHOWWINDOW;
-    si.wShowWindow = SW_HIDE;
+
 
     CreateProcessA(
         NULL,
-        (LPSTR)cmd,
+        (LPSTR)command,
         NULL,
         NULL,
         FALSE,
---
+++ RCWM/files/ExecutionFiles/RcwmExecutable-single.cpp
@@ -0,0 +1,83 @@
+#include <windows.h>
+#pragma comment(lib, "Advapi32.lib")
+#include <string>
+
+// cl /O2 /Os /nologo /Fe:rcwm-reg.exe RcwmExecutable.cpp /link  /OPT:REF /OPT:ICF Advapi32.lib  /SUBSYSTEM:WINDOWS
+// rcwm-reg.exe rgkey test
+// -> HKEY_CURRENT_USER\SOFTWARE\RCWM\rgkey
+
+//dlink
+//flink
+//miror
+//rmove
+//rcopy
+//rstrc
+
+
+/*
+void LogToFile(const char* message) {
+    const char* logPath = "C:\\Users\\root\\Desktop\\reg_log.txt";
+    HANDLE hFile = CreateFileA(logPath, FILE_APPEND_DATA, FILE_SHARE_READ,
+        NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (hFile != INVALID_HANDLE_VALUE) {
+        DWORD bytesWritten;
+        WriteFile(hFile, message, (DWORD)strlen(message), &bytesWritten, NULL);
+        WriteFile(hFile, "\r\n", 2, &bytesWritten, NULL);
+        CloseHandle(hFile);
+    }
+}
+*/
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR arg, int) {
+    // Registry operations
+    //HKEY hKey;
+    HKEY hSubKey;
+
+
+    std::string fullArg(arg);
+
+    std::string regKey = fullArg.substr(0, 5);
+    std::string directoryPath = fullArg.substr(6); // everything after first 5 chars and the whitespace
+
+    std::string regKeyPath = "Software\\RCWM\\" + regKey;
+    //const char* regKeyPath = "Software\\RCWM\\dl";
+
+    /*
+    LONG result = RegCreateKeyExA(HKEY_CURRENT_USER, keyPath, 0, NULL, 0, KEY_WRITE, NULL, &hKey, NULL);
+    //LogToFile("arg:");
+    //LogToFile(path);
+    if (result == ERROR_SUCCESS) {
+        //const char* valueData = "MyValueData";
+        result = RegSetValueExA(hKey, path, 0, REG_SZ, NULL, 0);
+        //LogToFile(result == ERROR_SUCCESS ? "Registry write successful" : "Registry write failed");
+        RegCloseKey(hKey);
+    }
+    else {
+        //LogToFile("Failed to open/create registry key");
+    }
+    */
+
+
+
+    //RegOpenKeyA(HKEY_CURRENT_USER, regKeyPath.c_str(), &hSubKey);
+    if (RegOpenKeyExA(HKEY_CURRENT_USER, regKeyPath.c_str(), 0, KEY_READ | KEY_WRITE, &hSubKey) == ERROR_SUCCESS) {
+        char valueName[256];
+        DWORD nameLen;
+        while (true) {
+            nameLen = sizeof(valueName);
+            if (RegEnumValueA(hSubKey, 0, valueName, &nameLen, NULL, NULL, NULL, NULL) != ERROR_SUCCESS)
+                break;
+            RegDeleteValueA(hSubKey, valueName);
+        }
+
+        RegSetValueExA(hSubKey, directoryPath.c_str(), 0, REG_NONE, NULL, 0);
+        RegCloseKey(hSubKey);
+    }
+
+
+  //  RegSetValueExA(hSubKey, directoryPath.c_str(), 0, REG_NONE, NULL, 0);
+    //LogToFile(result == ERROR_SUCCESS ? "Registry write successful" : "Registry write failed");
+   // RegCloseKey(hSubKey);
+
+
+    return 0;
+}
