5da6eafbc03cf646b248382440ac37d35316804b
GChuf <gasper.cefarin@gmail.com>
2025-07-11 22:32:44 +0200

    Win6 specific files for v3

--- RCWM/InstallerFiles/shortcuts6.ps1
+++ RCWM/InstallerFiles/shortcuts6.ps1
@@ -1,5 +1,5 @@
 $bytes = [System.IO.File]::ReadAllBytes(".\OSSpecificFiles\Win6\rcp.lnk")
-[System.IO.File]::WriteAllBytes(".\Temp\rcp.lnk", $bytes)
+[System.IO.File]::WriteAllBytes("C:\Windows\System32\rcp.lnk", $bytes)
 #$bytes = [System.IO.File]::ReadAllBytes(".\OSSpecificFiles\Win6\Win7NoBypass.lnk")
 #[System.IO.File]::WriteAllBytes(".\Temp\Win7NoBypass.lnk", $bytes)
 Write-Host "Generated shortcuts."
--- RCWM/files/OSSpecificFiles/Win6/rcp.cmd
+++ RCWM/files/OSSpecificFiles/Win6/rcp.cmd
@@ -1,3 +1,3 @@
 @echo off
 reg add HKCU\SOFTWARE\RCWM /v dir /t REG_MULTI_SZ /f /d %1 1>NUL
-powershell.exe C:\Program Files (x86)\RCWM\rcopy.lnk %2 %3
+powershell.exe C:\Windows\System32\rcp.lnk %2 %3
