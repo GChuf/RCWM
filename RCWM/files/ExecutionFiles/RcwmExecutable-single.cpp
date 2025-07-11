f2bec5fb11372a8e41269c351cc70d7c5387e5df
GChuf <gasper.cefarin@gmail.com>
2025-07-11 23:20:58 +0200

    fix encoding - working on win10

--- RCWM/files/ExecutionFiles/RcwmExecutable-single.cpp
+++ RCWM/files/ExecutionFiles/RcwmExecutable-single.cpp
@@ -1,83 +1,41 @@
-#include <windows.h>
-#pragma comment(lib, "Advapi32.lib")
+﻿#include <windows.h>
 #include <string>
 
-// cl /O2 /Os /nologo /Fe:rcwm-reg-single.exe RcwmExecutable-single.cpp /link  /OPT:REF /OPT:ICF Advapi32.lib  /SUBSYSTEM:WINDOWS
-// rcwm-reg.exe rgkey test
-// -> HKEY_CURRENT_USER\SOFTWARE\RCWM\rgkey
+#pragma comment(lib, "Advapi32.lib")
 
-//dlink
-//flink
-//miror
-//rcmov
-//rcopy
-//rstrc
-
-
-/*
-void LogToFile(const char* message) {
-    const char* logPath = "C:\\Users\\root\\Desktop\\reg_log.txt";
-    HANDLE hFile = CreateFileA(logPath, FILE_APPEND_DATA, FILE_SHARE_READ,
-        NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-    if (hFile != INVALID_HANDLE_VALUE) {
-        DWORD bytesWritten;
-        WriteFile(hFile, message, (DWORD)strlen(message), &bytesWritten, NULL);
-        WriteFile(hFile, "\r\n", 2, &bytesWritten, NULL);
-        CloseHandle(hFile);
-    }
-}
-*/
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR arg, int) {
-    // Registry operations
-    //HKEY hKey;
+int WINAPI wWinMain(HINSTANCE, HINSTANCE, LPWSTR arg, int) {
     HKEY hSubKey;
 
+    std::wstring fullArg(arg);  // Already Unicode (UTF-16)
 
-    std::string fullArg(arg);
-
-    std::string regKey = fullArg.substr(0, 5);
-    std::string directoryPath = fullArg.substr(6); // everything after first 5 chars and the whitespace
-
-    std::string regKeyPath = "Software\\RCWM\\" + regKey;
-    //const char* regKeyPath = "Software\\RCWM\\dl";
-
-    /*
-    LONG result = RegCreateKeyExA(HKEY_CURRENT_USER, keyPath, 0, NULL, 0, KEY_WRITE, NULL, &hKey, NULL);
-    //LogToFile("arg:");
-    //LogToFile(path);
-    if (result == ERROR_SUCCESS) {
-        //const char* valueData = "MyValueData";
-        result = RegSetValueExA(hKey, path, 0, REG_SZ, NULL, 0);
-        //LogToFile(result == ERROR_SUCCESS ? "Registry write successful" : "Registry write failed");
-        RegCloseKey(hKey);
+    if (fullArg.length() < 7 || fullArg[5] != L' ') {
+        return 1; // invalid format
     }
-    else {
-        //LogToFile("Failed to open/create registry key");
-    }
-    */
 
+    std::wstring regKey = fullArg.substr(0, 5);
+    std::wstring directoryPath = fullArg.substr(6);
 
+    std::wstring regKeyPath = L"Software\\RCWM\\" + regKey;
+    std::wstring valueName = directoryPath;
 
-    //RegOpenKeyA(HKEY_CURRENT_USER, regKeyPath.c_str(), &hSubKey);
-    if (RegOpenKeyExA(HKEY_CURRENT_USER, regKeyPath.c_str(), 0, KEY_READ | KEY_WRITE, &hSubKey) == ERROR_SUCCESS) {
-        char valueName[256];
+    if (RegOpenKeyExW(HKEY_CURRENT_USER, regKeyPath.c_str(), 0, KEY_READ | KEY_WRITE, &hSubKey) == ERROR_SUCCESS) {
+        // Delete all existing values
+        wchar_t nameBuf[256];
         DWORD nameLen;
         while (true) {
-            nameLen = sizeof(valueName);
-            if (RegEnumValueA(hSubKey, 0, valueName, &nameLen, NULL, NULL, NULL, NULL) != ERROR_SUCCESS)
+            nameLen = sizeof(nameBuf) / sizeof(wchar_t);
+            if (RegEnumValueW(hSubKey, 0, nameBuf, &nameLen, NULL, NULL, NULL, NULL) != ERROR_SUCCESS)
                 break;
-            RegDeleteValueA(hSubKey, valueName);
+            RegDeleteValueW(hSubKey, nameBuf);
         }
 
-        RegSetValueExA(hSubKey, directoryPath.c_str(), 0, REG_NONE, NULL, 0);
+        const wchar_t* dummyValue = L"Test čžš";
+        RegSetValueExW(hSubKey, valueName.c_str(), 0, REG_SZ,
+            reinterpret_cast<const BYTE*>(dummyValue),
+            (DWORD)((wcslen(dummyValue) + 1) * sizeof(wchar_t)));
+
         RegCloseKey(hSubKey);
     }
-
-
-  //  RegSetValueExA(hSubKey, directoryPath.c_str(), 0, REG_NONE, NULL, 0);
-    //LogToFile(result == ERROR_SUCCESS ? "Registry write successful" : "Registry write failed");
-   // RegCloseKey(hSubKey);
-
 
     return 0;
 }
