4d9334058c61d408652b534e3e0d46278f158908
GChuf <gasper.cefarin@gmail.com>
2025-07-12 10:00:07 +0200

    Add .exe files for storing into registry

--- RCWM/files/ExecutionFiles/RcwmExecutable-multiple.cpp
+++ RCWM/files/ExecutionFiles/RcwmExecutable-multiple.cpp
@@ -1,4 +1,4 @@
-﻿#include <windows.h>
+#include <windows.h>
 #include <string>
 
 #pragma comment(lib, "Advapi32.lib")
@@ -6,7 +6,7 @@
 //open x86 Native Tools Command Prompt for VS 2022
 //C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Visual Studio 2022\Visual Studio Tools\VC
 // 
-//cl /GL /O2 /Os /nologo /Fe:rcwm-single.exe RcwmExecutable-single.cpp /link /OPT:REF /OPT:ICF Advapi32.lib /SUBSYSTEM:WINDOWS /ENTRY:wWinMainCRTStartup
+//cl /GL /O2 /Os /nologo /Fe:rcwm-multiple.exe RcwmExecutable-multiple.cpp /link /OPT:REF /OPT:ICF Advapi32.lib /SUBSYSTEM:WINDOWS /ENTRY:wWinMainCRTStartup
 
 int WINAPI wWinMain(HINSTANCE, HINSTANCE, LPWSTR arg, int) {
     HKEY hSubKey;
@@ -18,19 +18,10 @@ int WINAPI wWinMain(HINSTANCE, HINSTANCE, LPWSTR arg, int) {
     }
 
     std::wstring regKey = fullArg.substr(0, 5);
-    std::wstring directoryPath = fullArg.substr(6);
+    std::wstring directoryPath = fullArg.substr(6); // after space
     std::wstring regKeyPath = L"Software\\RCWM\\" + regKey;
 
     if (RegOpenKeyExW(HKEY_CURRENT_USER, regKeyPath.c_str(), 0, KEY_READ | KEY_WRITE, &hSubKey) == ERROR_SUCCESS) {
-        // Delete all existing values
-        wchar_t nameBuf[256];
-        DWORD nameLen;
-        while (true) {
-            nameLen = sizeof(nameBuf) / sizeof(wchar_t);
-            if (RegEnumValueW(hSubKey, 0, nameBuf, &nameLen, NULL, NULL, NULL, NULL) != ERROR_SUCCESS)
-                break;
-            RegDeleteValueW(hSubKey, nameBuf);
-        }
         // Store registry value with REG_NONE and empty data
         RegSetValueExW(hSubKey, directoryPath.c_str(), 0, REG_NONE, NULL, 0);
         RegCloseKey(hSubKey);
