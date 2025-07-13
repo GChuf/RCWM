45711a1ca82179684e64f370c9f827c95ba64303
GChuf <gasper.cefarin@gmail.com>
2025-07-13 12:56:46 +0200

    fix typo

--- RCWM/InstallerFiles/Options.ps1
+++ RCWM/InstallerFiles/Options.ps1
@@ -29,7 +29,7 @@ $AddOptions = @(
 	New-Object PSObject -Property @{Name = 'Links'; RegFile = 'Links.reg'; Desc = 'Do you want to add symbolic/hard links'}
 	New-Object PSObject -Property @{Name = 'Logoff'; RegFile = 'Logoff.reg'; Desc = 'Do you want to add Sign Off to desktop background'}
 	New-Object PSObject -Property @{Name = 'Killall'; RegFile = 'Killall.reg'; Desc = 'Do you want to add Kill All to backgrounds'}
-	New-Object PSObject -Property @{Name = 'Mirror'; RegFile = 'Mirror.reg'; Desc = 'Do you want add RoboCopy Mirror option'}
+	New-Object PSObject -Property @{Name = 'Mirror'; RegFile = 'Mirror.reg'; Desc = 'Do you want to add RoboCopy Mirror option'}
 	New-Object PSObject -Property @{Name = 'MoveToFolder'; RegFile = 'MoveToFolder.reg'; Desc = 'Do you want to add Move To Folder'}
 	New-Object PSObject -Property @{Name = 'RCopyStructure'; RegFile = 'RCopyStructure.reg'; Desc = 'Do you want to add RoboCopy to copy Folder Structure only (exclude files)'}
 	New-Object PSObject -Property @{Name = 'RebootToRecovery'; RegFile = 'RebootToRecovery.reg'; Desc = 'Do you want to add Reboot to Recovery to "This PC"'}
