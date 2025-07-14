5a140e638e7a2b916885115ecba6b76755b94ea7
GChuf <gasper.cefarin@gmail.com>
2025-07-14 11:38:51 +0200

    Get rid of GL flags

--- RCWM/files/ExecutionFiles/RcwmExecutable-single.cpp
+++ RCWM/files/ExecutionFiles/RcwmExecutable-single.cpp
@@ -6,7 +6,7 @@
 //open x86 Native Tools Command Prompt for VS 2022
 //C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Visual Studio 2022\Visual Studio Tools\VC
 // 
-//cl /GL /O2 /Os /nologo /Fe:rcwm-single.exe RcwmExecutable-single.cpp /link /OPT:REF /OPT:ICF Advapi32.lib /SUBSYSTEM:WINDOWS /ENTRY:wWinMainCRTStartup
+//cl /O2 /Os /nologo /Fe:rcwm-single.exe RcwmExecutable-single.cpp /link /OPT:REF /OPT:ICF Advapi32.lib /SUBSYSTEM:WINDOWS /ENTRY:wWinMainCRTStartup
 
 int WINAPI wWinMain(HINSTANCE, HINSTANCE, LPWSTR arg, int) {
     HKEY hSubKey;
