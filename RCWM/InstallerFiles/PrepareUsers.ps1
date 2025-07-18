d27e84df198f36eaee48d93d84adb165d377a1c3
GChuf <gasper.cefarin@gmail.com>
2025-07-18 13:26:34 +0200

    Fix creation of reg keys for all users

--- RCWM/InstallerFiles/PrepareUsers.ps1
+++ RCWM/InstallerFiles/PrepareUsers.ps1
@@ -287,8 +287,7 @@ if ($mode1 -eq "A") {
 	#Copy RCWM_CreateRegistryKeys.bat file to %userprofile%\Start Menu\Programs\Startup so it executes on startup for users
 	#file deletes itself from user profile afterwards so that it doesn't keep executing at every login.
 	cd $initialLocation
-	New-Item -Path "$env:SystemDrive\Users\Default\Start Menu\Programs\Startup" -ItemType Directory | Out-Null
-	Copy-Item -Path "..\InstallerFiles\RCWM_CreateRegistryKeys.bat" -Destination "$env:SystemDrive\Users\Default\Start Menu\Programs\Startup" | Out-Null
+	Copy-Item -Path "..\InstallerFiles\RCWM_CreateRegistryKeys.bat" -Destination "$env:SystemDrive\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp" | Out-Null
 
 	LoopThroughUsers -mode "all" -users $users
 } elseif ($mode1 -eq "D" ) { 
--- RCWM/InstallerFiles/RCWM_CreateRegistryKeys.bat
+++ RCWM/InstallerFiles/RCWM_CreateRegistryKeys.bat
@@ -1,10 +1,8 @@
-reg add "HKEY_CURRENT_USER\SOFTWARE\RCWM" /f
-reg add "HKEY_CURRENT_USER\SOFTWARE\RCWM\dlink" /f
-reg add "HKEY_CURRENT_USER\SOFTWARE\RCWM\flink" /f
-reg add "HKEY_CURRENT_USER\SOFTWARE\RCWM\miror" /f
-reg add "HKEY_CURRENT_USER\SOFTWARE\RCWM\rcmov" /f
-reg add "HKEY_CURRENT_USER\SOFTWARE\RCWM\rcopy" /f
-reg add "HKEY_CURRENT_USER\SOFTWARE\RCWM\rstrc" /f
-
-rem delete the script itself
-del "%~f0"
+@echo off
+reg add "HKEY_CURRENT_USER\SOFTWARE\RCWM" /f >nul 2>&1
+reg add "HKEY_CURRENT_USER\SOFTWARE\RCWM\dlink" /f >nul 2>&1
+reg add "HKEY_CURRENT_USER\SOFTWARE\RCWM\flink" /f >nul 2>&1
+reg add "HKEY_CURRENT_USER\SOFTWARE\RCWM\miror" /f >nul 2>&1
+reg add "HKEY_CURRENT_USER\SOFTWARE\RCWM\rcmov" /f >nul 2>&1
+reg add "HKEY_CURRENT_USER\SOFTWARE\RCWM\rcopy" /f >nul 2>&1
+reg add "HKEY_CURRENT_USER\SOFTWARE\RCWM\rstrc" /f >nul 2>&1
