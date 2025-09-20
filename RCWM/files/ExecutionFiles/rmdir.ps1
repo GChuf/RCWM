abd1c5093c2d07267cb4bd787c8336535a845534
GChuf <gasper.cefarin@gmail.com>
2025-09-20 13:00:40 +0200

    add titles

--- RCWM/files/ExecutionFiles/rmdir.ps1
+++ RCWM/files/ExecutionFiles/rmdir.ps1
@@ -1,3 +1,9 @@
+[console]::InputEncoding = [text.utf8encoding]::UTF8
+[system.console]::OutputEncoding = [System.Text.Encoding]::UTF8
+
+$host.UI.RawUI.WindowTitle = "RCWM: Remove"
+Write-host "RCWM v3.0.0"
+
 Remove-Item $args[0] -Recurse
 if (-not $?) {
     Start-Sleep -Seconds 3
--- RCWM/files/ExecutionFiles/scpTo.ps1
+++ RCWM/files/ExecutionFiles/scpTo.ps1
@@ -1,3 +1,9 @@
+[console]::InputEncoding = [text.utf8encoding]::UTF8
+[system.console]::OutputEncoding = [System.Text.Encoding]::UTF8
+
+$host.UI.RawUI.WindowTitle = "RCWM: SCP"
+Write-host "RCWM v3.0.0"
+
 $item = '"' + $args[0] + '"'
 
 $user = Read-Host "Enter username"
