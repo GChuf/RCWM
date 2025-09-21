2ec3dbaa34bc61dbdfb89ee47c28b8d0799dc30a
GChuf <gasper.cefarin@gmail.com>
2025-09-21 22:31:15 +0200

    rmdir -> rmitem

--- RCWM/InstallerFiles/Options.ps1
+++ RCWM/InstallerFiles/Options.ps1
@@ -19,7 +19,7 @@ $AddOptions = @(
 	New-Object PSObject -Property @{Name = 'x'; RegFile = 'x'; Desc = 'Do you want to add RoboCopy files'; exception = "RCopy"}
 	New-Object PSObject -Property @{Name = 'x'; RegFile = 'x'; Desc = 'Do you want to add Move files (using robocopy)'; exception = "MvDir"}
 	New-Object PSObject -Property @{Name = 'PasteFromClipboard'; RegFile = 'PasteFromClipboard.reg'; Desc = 'Do you want to add Paste from Clipboard (using robocopy)'}
-	New-Object PSObject -Property @{Name = 'RmDir'; RegFile = 'RmDir.reg'; Desc = 'Do you want to add Remove Directory'}
+	New-Object PSObject -Property @{Name = 'RmItem'; RegFile = 'RmItem.reg'; Desc = 'Do you want to add Remove files'}
 	New-Object PSObject -Property @{Name = 'ScpFrom'; RegFile = 'ScpFrom.reg'; Desc = 'Do you want to add SCP from ...'}
 	New-Object PSObject -Property @{Name = 'ScpTo'; RegFile = 'ScpTo.reg'; Desc = 'Do you want to add SCP to ...'}
 	New-Object PSObject -Property @{Name = 'CMD'; RegFile = 'CMD.reg'; Desc = 'Do you want to add open CMD to background/folders/drives'}
