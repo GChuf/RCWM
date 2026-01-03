c7c96df005f37fba9bcdf85c2ddfc38857c15b5e
GChuf <gasper.cefarin@gmail.com>
2026-01-03 23:14:47 +0100

    re-add allUsers removed by mistake

--- RCWM/InstallerFiles/PrepareUsers.ps1
+++ RCWM/InstallerFiles/PrepareUsers.ps1
@@ -67,7 +67,7 @@ function loopThroughUsers() {
 	$sysDrive = $env:SystemDrive
 
 	#get all users from hklm
-	
+	$allUsers = Get-ChildItem -Path Registry::"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\S-1-5-21-*"| Select-Object Name
 
 	if ($allUsers.count -ge 2) {
 		Write-Host "Found " -NoNewLine; Write-Host $allUsers.Count -NoNewLine; " users in registry."
