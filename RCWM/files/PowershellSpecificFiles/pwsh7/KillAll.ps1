a8205d947636b821d66dfef662bdfc2a8dc06254
GChuf <gasper.cefarin@gmail.com>
2026-01-04 16:43:36 +0100

    Fix killall for pwsh7

--- RCWM/files/PowershellSpecificFiles/pwsh7/KillAll.ps1
+++ RCWM/files/PowershellSpecificFiles/pwsh7/KillAll.ps1
@@ -3,4 +3,4 @@ Write-host "RCWM v3.0.0"
 #get our own process' ID to filter it out
 $id = [System.Diagnostics.Process]::GetCurrentProcess() | Select-Object -ExpandProperty ID
 #killall
-(gps | ? {$_.mainwindowtitle}).Id | Where-Object {($_ -ne $id)}  | foreach-object -parallel {taskkill /f /pid $_}
+(gps | ? {$_.mainwindowtitle}).Id | Where-Object {($_ -ne $id)} | foreach-object -parallel {taskkill /f /pid $_}
