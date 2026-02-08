d1c54d69ef58e0d74dc726ce492f6a2358e255a6
GChuf <gasper.cefarin@gmail.com>
2026-02-08 12:41:31 +0100

    Fix rmitem and add rmitem.ps1 for win6 only

--- RCWM/files/OSSpecificFiles/Win6/rmitem.ps1
+++ RCWM/files/OSSpecificFiles/Win6/rmitem.ps1
@@ -1,3 +1,6 @@
+[console]::InputEncoding = [text.utf8encoding]::UTF8
+[system.console]::OutputEncoding = [System.Text.Encoding]::UTF8
+
 $host.UI.RawUI.WindowTitle = "RCWM: Remove"
 
 Remove-Item $args[0] -Recurse
