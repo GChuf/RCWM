c317a8ae640d6824a299191e1928f308d9a8ed61
GChuf <gasper.cefarin@gmail.com>
2026-08-06 13:45:28 +0200

    Add check for scp into drives
    fixes #31

--- RCWM/files/ExecutionFiles/scpFrom.ps1
+++ RCWM/files/ExecutionFiles/scpFrom.ps1
@@ -3,7 +3,19 @@
 
 $host.UI.RawUI.WindowTitle = "RCWM: SCP from ..."
 
-$dest = '"' + $args[0] + '"'
+$path = $args[0]
+
+# add trailing backslash for copying directly into drives
+if ($path.Length -eq 3 -and $path[2] -eq '"') {
+	$path = $path.substring(0,2)
+	$dest = '"' + $path + '\"'
+    $path += '\\'
+} else {
+	$path = '"' + $path + '"'
+	$dest = $path
+}
+
+
 
 $user = Read-Host "Enter username"
 $h0st = Read-Host "Enter source IP or hostname"
@@ -11,5 +23,5 @@ $src = Read-Host "Enter source file/folder"
 
 $command = $user + "@" + $h0st + ":" + $src
 Write-Host "Executing scp -r $command $dest"
-scp -r $command $dest
+scp -r $command $path
 start-sleep 1
