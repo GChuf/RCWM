f9f880c79e61fa76c81181dc8d26baf4c08a6c22
GChuf <gasper.cefarin@gmail.com>
2026-08-06 14:06:14 +0200

    filter out explorer.exe from killall

--- RCWM/files/ExecutionFiles/KillAll.ps1
+++ RCWM/files/ExecutionFiles/KillAll.ps1
@@ -2,5 +2,5 @@ $host.UI.RawUI.WindowTitle = "RCWM: Kill All"
 #get our own process' ID to filter it out
 $id = [System.Diagnostics.Process]::GetCurrentProcess() | Select-Object -ExpandProperty ID
 #killall
-#get all processes with visible main window
-(gps | ? {$_.mainwindowtitle}).Id | Where-Object {($_ -ne $id)}  | foreach-object -process {taskkill /f /pid $_}
+#get all processes with visible main window, except explorer
+(gps | ? {$_.mainwindowtitle -and $_.ProcessName -ne "explorer"}).Id | Where-Object {($_ -ne $id)} | foreach-object -process {taskkill /f /pid $_}
